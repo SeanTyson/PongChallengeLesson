e7ef90d80e46593383c62b9ada719660313caf12
Aine Survilaite <aine.survilaite@gmail.com>
2024-12-18 23:46:50 +0000

    added scoring

--- pong.py
+++ pong.py
@@ -5,7 +5,9 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+coolfondant = pygame.font.SysFont('Comic Sans MS', 30)
 dt = 0
+scores = [0,0]
 paddleHeight = 120
 paddleWidth = 30
 paddleRightEdgeX = 180
@@ -20,8 +22,18 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("black")
+    screen.fill("purple")
 
+    if ballPosition.x <= 0:
+        ballPosition.x = screen.get_width() / 2
+        ballPosition.y = screen.get_height() / 2
+        scores[0] += 1
+    
+
+    text_surface = coolfondant.render(str(scores[0]), False, (0, 0, 0))
+    screen.blit(text_surface, (screen.get_width() - 50,0))
+    text_surface = coolfondant.render(str(scores[1]), False, (0, 0, 0))
+    screen.blit(text_surface, (30,0))
     pygame.draw.circle(screen, "red", ballPosition, 15)
     paddle = pygame.Rect(paddlePosition['x'], paddlePosition['y'], paddleWidth, paddleHeight)
     pygame.draw.rect(screen, "red", paddle)
@@ -36,8 +48,10 @@ while running:
 
     if leftPaddleHit:
         ballPosition.x += 300 * dt
+        ballPosition.y -= 100 * dt
     else:
         ballPosition.x -= 300 * dt
+        ballPosition.y += 100 * dt
 
     #TODO: Make this dynamic so it will always be a pixel exactly in the range of the ballposition possible values
     ballEdgeX = ballPosition.x + 15
